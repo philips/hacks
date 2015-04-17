81816425967ab21b1c0af0fcef820fcede8c8b8a
Brandon Philips <brandon.philips@coreos.com>
2015-04-17 12:15:54 -0700

    main: use local filesystem copy
    Doing s3/gs is cool but if I lose internet the demo goes kaboom. Do this
    for now.

--- etcd-backup/main.go
+++ etcd-backup/main.go
@@ -1,134 +1,52 @@
-/*
-Copyright 2013 Google Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Binary storage-sample creates a new bucket, performs all of its operations
-// within that bucket, and then cleans up after itself if nothing fails along the way.
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
-	"net/http"
+	"io"
+	"io/ioutil"
 	"os"
-
-	"code.google.com/p/goauth2/oauth"
-	"code.google.com/p/google-api-go-client/storage/v1"
+	"path"
+	"time"
 )
 
-const (
-	fileName   = "/usr/share/dict/words" // The name of the local file to upload.
-	objectName = "english-dictionary"    // This can be changed to any valid object name.
-
-	// For the basic sample, these variables need not be changed.
-	scope       = storage.DevstorageFull_controlScope
-	authURL     = "https://accounts.google.com/o/oauth2/auth"
-	tokenURL    = "https://accounts.google.com/o/oauth2/token"
-	entityName  = "allUsers"
-	redirectURL = "urn:ietf:wg:oauth:2.0:oob"
-)
-
-var (
-	cacheFile = flag.String("cache", "cache.json", "Token cache file")
-	code      = flag.String("code", "", "Authorization Code")
-
-	// For additional help with OAuth2 setup,
-	// see http://goo.gl/cJ2OC and http://goo.gl/Y0os2
-
-)
+func cp(dst, src string) error {
+	s, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	// no need to check errors on read only file, we already got everything
+	// we need from the filesystem, so nothing can go wrong now.
+	defer s.Close()
+	d, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(d, s); err != nil {
+		d.Close()
+		return err
+	}
+	return d.Close()
+}
 
 func main() {
-	flag.Parse()
+	dataDir := os.Args[1]
+	backupDir := os.Args[2]
 
-	// Change these variable to match your personal information.
-	bucketName := os.Getenv("ETCD_BACKUP_BUCKET_NAME")
-	projectID := os.Getenv("ETCD_BACKUP_PROJECT_ID")
-	clientId := os.Getenv("ETCD_BACKUP_CLIENT_ID")
-	clientSecret := os.Getenv("ETCD_BACKUP_CLIENT_SECRET")
-
-	// Set up a configuration boilerplate.
-	config := &oauth.Config{
-		ClientId:     clientId,
-		ClientSecret: clientSecret,
-		Scope:        scope,
-		AuthURL:      authURL,
-		TokenURL:     tokenURL,
-		TokenCache:   oauth.CacheFile(*cacheFile),
-		RedirectURL:  redirectURL,
-	}
-
-	// Set up a transport using the config
-	transport := &oauth.Transport{
-		Config:    config,
-		Transport: http.DefaultTransport,
-	}
-
-	token, err := config.TokenCache.Token()
-	if err != nil {
-		if *code == "" {
-			url := config.AuthCodeURL("")
-			fmt.Println("Visit URL to get a code then run again with -code=YOUR_CODE")
-			fmt.Println(url)
-			os.Exit(1)
-		}
-
-		// Exchange auth code for access token
-		token, err = transport.Exchange(*code)
+	for {
+		info, err := ioutil.ReadDir(dataDir)
 		if err != nil {
-			log.Fatal("Exchange: ", err)
+			fmt.Printf("readdir: %v\n", err)
 		}
-		fmt.Printf("Token is cached in %v\n", config.TokenCache)
-	}
-	transport.Token = token
 
-	httpClient := transport.Client()
-	service, err := storage.New(httpClient)
-
-	// If the bucket already exists and the user has access, warn the user, but don't try to create it.
-	if _, err := service.Buckets.Get(bucketName).Do(); err == nil {
-		fmt.Printf("Bucket %s already exists - skipping buckets.insert call.", bucketName)
-	} else {
-		// Create a bucket.
-		if res, err := service.Buckets.Insert(projectID, &storage.Bucket{Name: bucketName}).Do(); err == nil {
-			fmt.Printf("Created bucket %v at location %v\n\n", res.Name, res.SelfLink)
-		} else {
-			log.Fatalf("Failed creating bucket %s: %v", bucketName, err)
+		for _, i := range info {
+			err = cp(path.Join(backupDir, i.Name()), path.Join(dataDir, i.Name()))
+			if err != nil {
+				fmt.Printf("cp %s: %v\n", i.Name(), err)
+				continue
+			}
+			fmt.Printf("cp %s: success\n", i.Name())
 		}
-	}
 
-	// List all buckets in a project.
-	if res, err := service.Buckets.List(projectID).Do(); err == nil {
-		fmt.Println("Buckets:")
-		for _, item := range res.Items {
-			fmt.Println(item.Id)
-		}
-		fmt.Println()
-	} else {
-		log.Fatalf("Buckets.List failed: %v", err)
-	}
-
-	// Insert an object into a bucket.
-	object := &storage.Object{Name: objectName}
-	file, err := os.Open(fileName)
-	if err != nil {
-		log.Fatalf("Error opening %q: %v", fileName, err)
-	}
-	if res, err := service.Objects.Insert(bucketName, object).Media(file).Do(); err == nil {
-		fmt.Printf("Created object %v at location %v\n\n", res.Name, res.SelfLink)
-	} else {
-		log.Fatalf("Objects.Insert failed: %v", err)
+		time.Sleep(time.Second)
 	}
 }
